99691cfc987bad35e3b8c898cc9646ccd6d606a6
korede <lateefolawuyi20@gmail.com>
2024-03-28 12:37:28 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,129 @@
+//2
+// let multiplier = 5
+
+// for(let i = 0; i >= 12; i++ ){
+//     console.log(`${multipl} * ${i}` = `${multiply * i}`);
+
+// }
+
+
+
+//7
+
+const school = {
+    name: "lateef",
+    location: "lagos",
+    students: [
+        {
+            name: "adebisi",
+            age: "15",
+            grade: "A"
+        },
+        {
+            name: "juwon",
+            age: "32",
+            grade: "B"
+        },
+        {
+            name: "oba",
+            age: "50",
+            grade: "C"
+
+        }
+    ]
+}
+
+console.log(school);
+
+
+//4
+
+let likes = [10,20,15,30]
+let comments = [5,10,8,15]
+
+function calculateTotal(likes, comments) {
+    let totalEngagement = []; 
+
+    for( let i = 0; i < likes.length; i++){
+       let engagemnt = likes[i] + comments[i] 
+       totalEngagement.push(engagemnt)
+
+       return totalEngagement
+    }
+}
+
+const totalEngagement = calculateTotal(likes, comments)
+console.log("total enagement for each post = :", totalEngagement);
+
+
+// let count = 1
+// for( let i = 1; i <= 20; i++){
+    
+// }
+
+
+class Animal {
+    constructor(name,age,species){
+        this.name = "cindy";
+        this.age = 3;
+        this.species = "lhasa"
+
+    }
+    
+    eat(){
+
+    }
+
+    sleep(){
+
+    }
+}
+
+let animalType = new animalSpecie()
+console.log(animal);
+
+
+
+//6
+const grade = [100]
+if(grade >= 90){
+    console.log("A");
+}else if(grade >= 70){
+    console.log("B");
+}else if(grade >= 55){
+    console.log("C");
+}else if(grade >= 40){
+    console.log("D");
+}else{
+    console.log("F");
+}
+
+//switch case
+
+const grades = [100]
+switch(list){
+    case grade >= 90:
+        console.log("A");
+        break
+    case grade >= 70:
+        console.log("B");
+        break
+    case grade >= 55:
+        console.log("C");
+        break
+    case grade >= 40:
+        console.log("D");
+    default:
+        console.log("F");
+}
+
+
+
+//1
+
+//to do list
+
+//DOM
+
+document.addEventListener
+
